bab923799c949f331ed9a4bbd755a9fb00b571aa
Ken Murchison <murch@fastmail.com>
2018-10-25 14:24:13 -0400

    mailbox.c: use dlist to store cyrus.header data, including mboxname and mbtype

--- imap/mailbox.h
+++ imap/mailbox.h
@@ -693,4 +693,6 @@ extern struct conversations_state *mailbox_get_cstate(struct mailbox *mailbox);
 typedef void mailbox_wait_cb_t(void *rock);
 extern void mailbox_set_wait_cb(mailbox_wait_cb_t *cb, void *rock);
 
+extern struct dlist *mailbox_acl_to_dlist(const char *aclstr);
+
 #endif /* INCLUDED_MAILBOX_H */
